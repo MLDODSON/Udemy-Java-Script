0c3850599b35aeec9ba7f3c776ea7dc5d8d5b92f
MLDODSON <mldodson@fullsail.edu>
2017-01-16 18:48:45 -0600

    operators 1.1

---
+++ operators.js
@@ -0,0 +1,52 @@
+/*Lecture: variables
+var name = "Mike";
+console.log(name);
+
+var lastName = "Dodson";
+console.log(lastName);
+
+var age = 35;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// Lecture: variables 2
+/*
+var name = "Mike";
+var age = 35;
+//console.log(name + age);
+//console.log(age + age);
+
+var job, isMarried;
+//console.log(job);
+
+job = "web developer";
+isMarried = true;
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = "forty two";
+job = "graphic artist";
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+//var lastName = prompt("What is the last name?");
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+*/
+
+// Lecture: operators
+var now = 2017;
+var birthYear = now - 42;
+birthYear = now - 42 * 2;
+//2017 - 84
+//1933
+console.log(birthYear);
+
+var ageGlenn = 30;
+var ageJane = 30;
+
+ageGlenn = (3 + 5) * 4 - 6;
+console.log(ageGlenn);
+
