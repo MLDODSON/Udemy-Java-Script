70f1479f10fa3aa757c822b3504c92679d3d5078
MLDODSON <mldodson@fullsail.edu>
2017-01-16 18:26:35 -0600

    var 2.2

--- var.js
+++ var.js
@@ -15,12 +15,19 @@ console.log(fullAge);
 // Lecture: variables 2
 var name = "Mike";
 var age = 35;
-console.log(name + age);
-console.log(age + age);
+//console.log(name + age);
+//console.log(age + age);
 
 var job, isMarried;
-console.log(job);
+//console.log(job);
 
 job = "web developer";
 isMarried = true;
 console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = "forty two";
+job = "graphic artist";
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt("What is the last name?");
+console.log(lastName);
