41c8a56f085012af3468dac87e888555a1f80b90
MLDODSON <mldodson@fullsail.edu>
2017-01-16 19:20:28 -0600

    if/else1.1

---
+++ ifelse.js
@@ -0,0 +1,108 @@
+/*Lecture: variables
+var name = "Mike";
+console.log(name);
+
+var lastName = "Dodson";
+console.log(lastName);
+
+var age = 35;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// Lecture: variables 2
+/*
+var name = "Mike";
+var age = 35;
+//console.log(name + age);
+//console.log(age + age);
+
+var job, isMarried;
+//console.log(job);
+
+job = "web developer";
+isMarried = true;
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = "forty two";
+job = "graphic artist";
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+//var lastName = prompt("What is the last name?");
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+*/
+
+// Lecture: operators
+/*
+var now = 2017;
+var birthYear = now - 42;
+birthYear = now - 42 * 2;
+//2017 - 84
+//1933
+console.log(birthYear);
+
+var ageGlenn = 30;
+var ageJane = 30;
+
+ageGlenn = ageJane = (3 + 5) * 4 - 6;
+//ageGlenn = ageJane = 26
+//ageGlenn = 26
+
+ageGlenn++;
+ageJane *= 2; 
+console.log(ageGlenn);
+console.log(ageJane);
+*/
+
+//Lecture: if/else statements
+
+var name = 'Mike';
+var age = 42;
+var isMarried = 'yes';
+
+if (isMarried === 'yes') {
+    console.log(name + ' is married!');
+} else {
+    console.log(name + ' will marry very soon :)');
+}
+
+isMarried = false;
+
+if(isMarried) {
+    console.log('YES!');
+} else {
+    console.log('NO!');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
