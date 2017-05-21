232bd90f2837b7c518641a53bca5d6a747d4c9c2
MLDODSON <mldodson@fullsail.edu>
2017-05-21 14:10:48 -0500

    starting var usage

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Time to get paid!");
+var name = "Mike";
+console.log(name);
+
+var lastName = "Greene";
+console.log(lastName);
+
+var age = 38;
+console.log(38);
+
+var eyeColor = "brown";
+console.log(eyeColor);
+
+var fullAge = true;
+console.log(fullAge);
