13ed55798bc8d6a14d224e67a010ac2f11fe6e1e
MLDODSON <mldodson@fullsail.edu>
2017-08-17 16:55:46 -0500

    practice

---
+++ functions.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+function numbers(a, b) {
+	console.log(a+b);
+}
+numbers(8,4);
---
+++ objects.js
@@ -0,0 +1,34 @@
+var team = {
+    name: "Cardinals",
+    city: "St. Louis",
+    color: "red",
+};
+
+//How to update an object. We will update the city.
+team.city = "Phoenix";
+
+//make an empty object and then add to it
+var person = {}
+person.name = "Paula";
+person.age = 20
+person.city = "Columbia";
+
+//all at one
+var person = {
+	name: "Paula",
+	age: 20,
+	city: "Columbia"
+};
+
+//amother way of initializing an Object
+var person = new Object();
+person.name = "Paula";
+person.age = 20;
+person.city = "Columbia";
+
+//Objects can hold all sorts of data
+
+//Objects are not bound to any particular order and have no start or finish like an array
+
+person.weight = 110;
+//This would add to the object
