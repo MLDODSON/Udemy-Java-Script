f20535f944121aacf7131fcda1ea9399a56f1acb
MLDODSON <mldodson@fullsail.edu>
2017-11-15 22:02:07 -0600

    data types

---
+++ basics.js
@@ -0,0 +1,14 @@
+// JavaScript Basics: Variables and data types
+//JS has 5 primitive data types: number, string, boolean, null and undefined
+
+var name = "Barbara";
+console.log(name + " is a fine person.");
+
+var lastName = "Johnson";
+console.log(name + " " + lastName + " makes the best cookies!");
+
+var age = 60;
+console.log("My aunt, " + name + " " + lastName + " is " + age + " years old.");
+
+var drinkingAge = true;
+console.log(drinkingAge);
