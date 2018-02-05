3116d843e02e5fe9e8e0b81451d260ddaa5f1956
MLDODSON <mldodson@fullsail.edu>
2018-02-05 14:11:48 -0600

    budget app updates

--- budget/app.js
+++ budget/app.js
@@ -1,21 +1,14 @@
 //module pattern
+
+// BUDGET CONTROLLER
 var budgetController = (function() {
 	
-	var x = 23;
-	
-	var add = function(a) {
-		return x + a;
-	};
-	
-	return {
-		publicTest: function(b) {
-			return add(b);
-		}
-	};
+	//Some code later
 	
 })();
 
 
+// UI CONTROLLER
 var UIController = (function() {
 	
 	//Some code later
@@ -23,14 +16,39 @@ var UIController = (function() {
 })();
 
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 	
-	var z = budgetCtrl.publicTest(5);
-	
-	return {
-		anotherPublic: function() {
-			console.log(z);
-		}
+	var ctrlAddItem = function() {
+		
+		// 1. Get  the filed input.
+		
+		// 2. Add the item to the budget controller.
+		
+		// 3. Add the item to the UI
+		
+		// 4. Calculate the budget
+		
+		// 5. Display the budget on the UI
+		
+		console.log("It works!");
+		
 	};
 	
+	document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+		
+	
+	document.addEventListener("keypress", function(event) {
+		
+		if (event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+		
+	});
+	
 })(budgetController, UIController);
+
+
+
+
+
