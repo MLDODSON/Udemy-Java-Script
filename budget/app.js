ddc78c0917980583dd7db0e65451ba0a643a17f4
MLDODSON <mldodson@fullsail.edu>
2018-02-08 11:34:33 -0600

    updating the budget: controller

--- budget/app.js
+++ budget/app.js
@@ -85,7 +85,7 @@ var UIController = (function() {
 			return {
 				type: document.querySelector(DOMstrings.inputType).value, // Will either be inc or exp
 				description: document.querySelector(DOMstrings.inputDescription).value,
-				value: document.querySelector(DOMstrings.inputValue).value
+				value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
 			};
 		
 		},
@@ -155,7 +155,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 	    });
 	};
 	
-	
+	var updateBudget = function() {
+		
+		// 1. Calculate the budget
+		
+		// 2. Return the budget
+		
+		// 3. Display the budget on the UI
+		
+	};
 	
 	var ctrlAddItem = function() {
 		var input, newItem;
@@ -163,18 +171,21 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 1. Get  the filed input.
 		input = UICtrl.getInput();
 		
-		// 2. Add the item to the budget controller.
-		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+		if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+			// 2. Add the item to the budget controller.
+			newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 		
-		// 3. Add the item to the UI
-		UICtrl.addListItem(newItem, input.type);
+			// 3. Add the item to the UI
+			UICtrl.addListItem(newItem, input.type);
 		
-		// 4. Clear the fields
-		UICtrl.clearFields();
+			// 4. Clear the fields
+			UICtrl.clearFields();
 		
-		// 5. Calculate the budget
+			// 5. Calculate and update budget
+			updateBudget();
+			
+		}
 		
-		// 6. Display the budget on the UI
 		
 		
 	};
