545c7c942d4b1c93383142f6ff1e77355c99f78c
MLDODSON <mldodson@fullsail.edu>
2018-03-17 14:38:33 -0500

    Percentage Calculator

--- budget/app.js
+++ budget/app.js
@@ -250,6 +250,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 		
 	};
 	
+	var updatepercentages = function() {
+		
+		// 1. Calculate percentages
+		
+		// 2. Read percentages from the budget controller
+		
+		// 3. Update the UI with the new percentages
+		
+	};
+	
 	var ctrlAddItem = function() {
 		var input, newItem;
 		
@@ -268,6 +278,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 		
 			// 5. Calculate and update budget
 			updateBudget();
+			
+			// 6. Calculate and update the percentages
+			updatepercentages();
 			
 		}
 		
@@ -296,6 +309,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 			// 3. Update and show the new budget
 			updateBudget();
 		}
+		
+		    // 4. Calculate and update the percentages
+			updatepercentages();
 	};
 	
 	return  {
