15beb76ed283f75984de11025cb9dafa3e37a6cd
MLDODSON <mldodson@fullsail.edu>
2018-04-22 12:39:23 -0500

    Learning DOM Manipulation

--- JS2018/dom.js
+++ JS2018/dom.js
@@ -1,1 +1,40 @@
-document.body.style.backgroundColor = "#e8f1f7";
+document.body.style.backgroundColor = "#e3f0f9";
+document.body.style.textAlign = "center";
+document.body.style.paddingLeft = "100px";
+document.body.style.paddingRight = "100px";
+
+document.getElementById("title");
+document.getElementById("teams");
+document.getElementById("finish");
+document.getElementById("finish2");
+
+
+var head = document.getElementById("title");
+var fball = document.getElementById("teams");
+var order = document.getElementById("finish");
+var order2 = document.getElementById("finish2");
+
+
+head.style.color = "#032a7f";
+head.style.fontSize = "40pt";
+head.style.marginTop = "150px";
+head.style.fontFamily = "Roboto";
+head.style.fontWeight = "bold";
+fball.innerHTML = "THE NFC WEST";
+fball.style.color = "#571ab2";
+fball.style.fontSize = "30pt";
+fball.style.fontFamily = "Roboto";
+fball.style.fontWeight = "bold";
+order.style.color = "black";
+order.style.fontSize = "24pt";
+order.style.paddingTop = "30px";
+order.style.fontFamily = "Roboto";
+order2.style.color = "#ef042b";
+order2.style.fontSize = "20pt";
+order2.style.fontFamily = "Roboto";
+order2.style.textAlign = "left";
+order2.style.paddingLeft = "450px";
+order2.innerHTML = order2.innerHTML.toUpperCase();
+
+
+
