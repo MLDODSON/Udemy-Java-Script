fea8213b398582b1b135c60640d3730418f11b9e
MLDODSON <mldodson@fullsail.edu>
2018-04-23 09:20:26 -0500

    Infinity project

---
+++ JS2018/INFINITY/infi.js
@@ -0,0 +1,14 @@
+
+
+var h1 = document.querySelector("h1");
+var h2 = document.querySelector("h2");
+
+h1.classList.add("gem");
+h2.classList.add("doit");
+
+document.getElementById("gem2");
+
+var stone = document.getElementById("gem2");
+stone.innerHTML = stone.innerHTML.toUpperCase();
+stone.style.marginTop = "450px";
+
