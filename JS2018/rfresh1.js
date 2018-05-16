d7b164b814e63aa13d37fd53e36adb0646ccd41f
MLDODSON <mldodson@fullsail.edu>
2018-05-16 15:00:28 -0500

    js refreshers

---
+++ JS2018/rfresh1.js
@@ -0,0 +1,29 @@
+var eats = ["Golden Coral", "Best Steak House", "Steak & Shake", " and Applebees"];
+
+console.log(`Some of my favorite places to eat are ${eats}`);
+
+function players(i) {
+	console.log(`My favorite athletes of all time are, ${i}.`);
+}
+players(["Dr. J", "Moses Malone", "Chocolate Thunder", "Otis Anderson", " and Marshall Faulk"]);
+
+
+function sum(a,b) {
+	console.log(a * b);
+}
+sum(8,3);
+
+var Sue = 100;
+var Lucy = 86;
+var place = "Red Lobster"; 
+	if(Sue > Lucy) {
+		console.log(`Susie is paying for the food at ${place}`);
+	} else {
+		console.log(`Lucille is paying for the food at ${place}`);
+	}
+
+function teams(c,d) {
+	console.log(`Some of the greatest sports franchises ever are the ${c} and the ${d}.`);
+}
+teams("Celtics", "Patriots");
+
