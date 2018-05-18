2d217440dae66cb02fc4edbca0ce9b1fd50785bd
MLDODSON <mldodson@fullsail.edu>
2018-05-18 16:49:48 -0500

    facts list/1st jQuery project

---
+++ JS2018/faq.js
@@ -0,0 +1,48 @@
+function insertQuestions()
+{
+	var xmlhttp = new XMLHttpRequest();
+	var url = "faq_list.json";
+	
+	xmlhttp.onreadystatechange = function()
+	{
+		if(xmlhttp.readyState === 4 && xmlhttp.status === 200) 
+		{
+			var myJsonObjectArray = JSON.parse(xmlhttp.responseText);
+			var html_text = "";
+			for (var i in myJsonObjectArray)
+			{
+				var template = list_item_template;
+				template = replaceAll(template, "{id}", myJsonObjectArray[i].id);
+				template = replaceAll(template, "{question}", myJsonObjectArray[i].question);
+				template = replaceAll(template, "{answer}", myJsonObjectArray[i].answer);
+				html_text += template;
+			}
+			document.getElementById("question_list").innerHTML = html_text;
+		}
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+
+
+
+
+
+
+function onClickShowAll()
+{
+	var questions = document.getElementById("question_list").children;
+	for(var i = 0; i < questions.length; i++)
+		{
+			questions[i].children[1].style.display = "inline-block";
+		}
+}
+
+function onClickHideAll()
+{
+	var questions = document.getElementById("question_list").children;
+	for(var i = 0; i < questions.length; i++)
+		{
+			questions[i].children[i].style.display = "none";
+		}
+}
